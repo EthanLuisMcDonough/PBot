156466f4b216cb17fffe306bb76031868590130b
Ethan Luis McDonough <elmcdonough@comcast.net>
2018-04-13 12:26:58 -0500

    Load credentials from config

--- build.sbt
+++ build.sbt
@@ -15,5 +15,6 @@ mainClass in (Compile, run) := Some("com.ethanmcdonough.PBot.Main")
 
 libraryDependencies ++= Seq(
 	"com.typesafe.play" %% "play-json" % "2.6.9",
-	"org.scalaj" %% "scalaj-http" % "2.3.0"	
+	"org.scalaj" %% "scalaj-http" % "2.3.0",
+    "com.typesafe" % "config" % "1.3.3"
 )
--- src/main/scala/com/ethanmcdonough/PBot/Main.scala
+++ src/main/scala/com/ethanmcdonough/PBot/Main.scala
@@ -1,8 +1,13 @@
 package com.ethanmcdonough.PBot
 
+import com.typesafe.config.{ Config, ConfigFactory }
+
 object Main {
   def main(args: Array[String]): Unit = {
-    val user = User(Identifier(args(0)), args(1))
+    val config: Config = ConfigFactory.load("credentials")
+    val identifier: Identifier = Identifier(config.getString("pbot.identifier"))
+    val password: String = config.getString("pbot.password")
+    val user: User = User(identifier, password)
     val session: Session = user.login.get
     println(session.csrfToken)
     println(session.commentOn(6561461926133760l, System.currentTimeMillis + "ms"))
